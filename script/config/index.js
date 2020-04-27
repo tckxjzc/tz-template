35b4b7c464b9ac2483cf2b6353819726010dd94d
tckxjzc <tckxjzc@outlook.com>
2020-04-27 18:48:11 +0800

    init

---
+++ script/config/index.js
@@ -0,0 +1,74 @@
+import path from 'path';
+import webpack from "webpack";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import argv from "./argv.js";
+import {enabledCopyPlugin} from './file.js';
+import {filterEntry, filterTemplate} from "../utils/index.js";
+
+const {__dirname} = argv;
+
+
+//file entry
+export const entry = filterEntry({
+    'index': path.resolve(__dirname, './src/index'),
+    'test': path.resolve(__dirname, './test/index'),
+});
+
+
+const baseTemplate = path.resolve(__dirname, './template/index.ejs');
+//dev output
+const devHtmlList = [
+    new HtmlWebpackPlugin({
+        template: baseTemplate,
+        filename: `index.html`,
+        inject: false,
+        chunks: ['index'],
+        chunksSortMode: 'manual'
+    })
+];
+//prods output
+const prodsHtmlList = [
+    new HtmlWebpackPlugin({
+        template: baseTemplate,
+        filename: `index.html`,
+        inject: false,
+        chunks: ['index'],
+        chunksSortMode: 'manual'
+    })
+];
+
+const htmlList = filterTemplate(argv.dev ? devHtmlList : prodsHtmlList);
+
+const pluginList = [...htmlList];
+//copy files
+if (enabledCopyPlugin && !argv.dev) {
+    pluginList.push(
+        new CopyWebpackPlugin([
+            {
+                from: path.resolve(__dirname, './static'),
+                to: path.resolve(__dirname, './dist')
+            }
+        ])
+    );
+}
+//plugins
+export const plugins = [
+    new webpack.DefinePlugin({
+        'DEVELOPMENT': JSON.stringify(argv.dev)
+    }),
+    new webpack.NamedModulesPlugin(),
+    ...pluginList
+];
+
+//externals
+export const externals = {
+    // 'react': 'React',
+    // 'react-dom': 'ReactDOM',
+    // 'react-router-dom': 'ReactRouterDOM',
+    // 'jquery': '$',
+    // 'moment': 'moment',
+    // 'swiper':'Swiper',
+    // 'mobx': 'mobx',
+    // 'mobx-react': 'mobxReact',
+};
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+
+if (DEVELOPMENT) {
+    console.log(`start----{dev:${DEVELOPMENT}}`);
+}
